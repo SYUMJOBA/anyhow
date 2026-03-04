f39ac7885742e5a9120402f2cffe32bd84d7265d
SYUMJOBA <syumjoba@gmail.com>
2026-03-04 13:56:23 +0100

    Feat: with_ like helpers

--- src/anyhow/complex/service.rs
+++ src/anyhow/complex/service.rs
@@ -1,5 +1,6 @@
 use super::Complex;
 use super::Value;
+use super::super::error::Error;
 
 use std::sync::Mutex;
 use std::sync::MutexGuard;
@@ -16,6 +17,12 @@ fn get_next_id() -> usize {
 static BUFFER: OnceLock<Mutex<Vec<Arc<Mutex<Complex>>>>> = OnceLock::new();
 fn get_buffer() -> MutexGuard<'static, Vec<Arc<Mutex<Complex>>>> {
     BUFFER.get_or_init(|| Mutex::new(Vec::new())).lock().unwrap_or_else(|poison| poison.into_inner())
+}
+
+fn with_complex<F, R>(id: usize, f: F) -> Result<R, Error> where F: FnOnce(&mut Complex) -> R {
+    let c = Service::get_complex_by_id(id)?.clone();
+    let mut c = c.lock().unwrap_or_else(|posion| posion.into_inner());
+    Ok(f(&mut *c))
 }
 
 pub struct Service;
