39a42bc5005c4f9c1fccdae703ff8c4847cdb125
SYUMJOBA <syumjoba@gmail.com>
2026-03-04 19:54:40 +0100

    Feat: foreign pointers

--- src/anyhow/complex.rs
+++ src/anyhow/complex.rs
@@ -5,11 +5,51 @@ mod service;
 pub(super) use service::Service as Service;
 
 #[derive(Clone, Debug)]
+struct ForeignComplexPointer {
+    pub(super) schema_id: usize,
+    pub(super) table_id: usize
+}
+
+#[derive(Clone, Debug)]
+struct ForeignOptionalAnyComplexPointer {
+    pub(super) schema_id: Option<usize>,
+    pub(super) table_id: Option<usize>
+}
+
+#[derive(Clone, Debug)]
+struct ForeignOptionalComplexPointer {
+    pub(super) schema_id: usize,
+    pub(super) table_id: Option<usize>
+}
+
+#[derive(Clone, Debug)]
+struct ForeignSelectComplexPointer {
+    pub(super) accepted_schemas: Vec<usize>,
+    pub(super) schema_id: usize,
+    pub(super) table_id: usize
+}
+
+#[derive(Clone, Debug)]
+struct ForeignOptionalSelectComplexPointer {
+    pub(super) accepted_schemas: Vec<usize>,
+    pub(super) schema_id: usize,
+    pub(super) table_id: Option<usize>
+}
+
+#[derive(Clone, Debug)]
 pub enum Value {
     Integer(i32),
     Float(f32),
     String(String),
-    Complex(usize) // table id
+    Complex(ForeignComplexPointer),
+    AnyComplex(ForeignComplexPointer),
+    SelectComplex(ForeignSelectComplexPointer), // table id
+    NullableInteger(Option<i32>),
+    NullableFloat(Option<f32>),
+    NullableString(Option<String>),
+    NullableComplex(ForeignOptionalComplexPointer),
+    NullableAnyComplex(ForeignOptionalAnyComplexPointer),
+    NullableSelectComplex(ForeignOptionalSelectComplexPointer),
 }
 
 #[derive(Clone, Debug)]
