3c132a2f294c3a52284767c7af2066de3bdf29a7
SYUMJOBA <syumjoba@gmail.com>
2026-03-05 00:59:51 +0100

    Fix: duplicated ids now detected

--- src/anyhow/verifier.rs
+++ src/anyhow/verifier.rs
@@ -6,21 +6,28 @@ use super::*;
 pub struct Service;
 
 impl Service {
-    fn verify_schema(schema: &ComplexSchema, complex: &Complex) -> Result<(), ComplexProcessError> {
-        if schema.schema.len() != complex.members.len() {
-            return Err(ComplexProcessError::InvalidFormat)
+    pub fn verify_schema(schema: &ComplexSchema, members: &Vec<Member>) -> Result<(), ComplexProcessError> {
+            if schema.schema.len() != members.len() {
+                return Err(ComplexProcessError::InvalidFormat)
+            }
+
+            let mut checked_ids = Vec::new();
+
+            for item in schema.schema.iter() {
+                let id = item.id;
+                let m = members.iter().find(|m| m.id == id).ok_or(ComplexProcessError::InvalidFormat)?;
+                Self::is_same_kind(&item, &m.value)?;
+                checked_ids.push(id);
+            }
+
+            if checked_ids.len() == schema.schema.len() {
+                return Ok(())
+            }
+
+            Err(ComplexProcessError::InvalidFormat)
         }
 
-        for item in schema.schema.iter() {
-            let id = item.id;
-            let m = complex.get_member_by_id(id)?;
-            Self::is_same_kind(&item, &m.value)?;
-        }
-
-        Ok(())
-    }
-
-    fn is_same_kind(schema: &ValueSchema, value: &Value) -> Result<(), ComplexProcessError> {
+    pub fn is_same_kind(schema: &ValueSchema, value: &Value) -> Result<(), ComplexProcessError> {
         if ValueKind::is_semantically_coherent(value, schema) {
             return Ok(())
         }
